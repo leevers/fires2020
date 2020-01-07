0da1a5f531d44541368a8447f24bec60b9aab52d
leevers <leevers@gmail.com>
2020-01-07 11:27:03 +1100

    Add legends change visualisation ofhotspot data

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var global = {
     map: null
 }
 
-require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseElevationLayer","esri/layers/ElevationLayer", "esri/layers/FeatureLayer", "esri/layers/WMSLayer", "esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/Point", "esri/widgets/LayerList", "esri/widgets/BasemapToggle", "esri/widgets/Popup", "esri/geometry/SpatialReference", "esri/geometry/coordinateFormatter"],
- function (Map, WebMap, MapView, BaseElevationLayer, ElevationLayer, FeatureLayer, WMSLayer, GraphicsLayer, Graphic, Point, LayerList, BasemapToggle, Popup, SpatialReference, coordinateFormatter)
+require(["esri/Map", "esri/WebMap", "esri/views/SceneView", "esri/views/MapView", "esri/layers/BaseElevationLayer","esri/layers/ElevationLayer", "esri/layers/FeatureLayer", "esri/layers/WMSLayer", "esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/Point", "esri/renderers/smartMapping/creators/relationship", "esri/widgets/LayerList", "esri/widgets/BasemapToggle", "esri/widgets/Popup", "esri/geometry/SpatialReference", "esri/geometry/coordinateFormatter", "esri/geometry/projection"],
+ function (Map, WebMap, SceneView, MapView, BaseElevationLayer, ElevationLayer, FeatureLayer, WMSLayer, GraphicsLayer, Graphic, Point, relationshipRendererCreator, LayerList, BasemapToggle, Popup, SpatialReference, coordinateFormatter, projection)
 {
     //  var ExaggeratedElevationLayer = BaseElevationLayer.createSubclass({
     //      properties: {
@@ -89,7 +89,15 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseEleva
                             id: "decrease-opacity"
                         }
                     ]
-                ]
+                ];
+
+                if (item.layer.legendEnabled)
+                {
+                    item.panel = {
+                        content: "legend",
+                        open: false
+                    };
+                }
             }
         }
     });
@@ -172,6 +180,7 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseEleva
             title: "Hotspot Data (Sentinel)",
             url: "https://sentinel.ga.gov.au/geoserver/public/wms",
             listMode: "hide-children",
+            visible: false,
             sublayers: [{
                 title: "Current Hotspots",
                 name: "hotspot_current"
@@ -181,50 +190,197 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseEleva
         global.map.add(hotspotLayer);
 
         // https://livefeeds.arcgis.com/arcgis/rest/services/LiveFeeds/NOAA_METAR_current_wind_speed_direction/MapServer/0
-        const noaaMetarLayer = new FeatureLayer({
+        var noaaMetarLayer = new FeatureLayer({
             // URL to the service
-            url: "https://livefeeds.arcgis.com/arcgis/rest/services/LiveFeeds/NOAA_METAR_current_wind_speed_direction/MapServer/0",
+            // url: "https://livefeeds.arcgis.com/arcgis/rest/services/LiveFeeds/NOAA_METAR_current_wind_speed_direction/MapServer/0",
+            url: "https://services9.arcgis.com/RHVPKKiFTONKtxq3/ArcGIS/rest/services/NOAA_METAR_current_wind_speed_direction_v1/FeatureServer/0",
+            popupTemplate: {
+                title: "NOAA METAR Wind",
+                content: "Station: {STATION_NAME}<br>" + 
+                    "Direction: {WIND_DIRECT}<br>" + 
+                    "Speed: {WIND_SPEED}<br>" + 
+                    "Gust: {WIND_GUST}<br>" + 
+                    "MSLP: {PRESSURE}<br>" + 
+                    "OBS_DATETIME: {OBS_DATETIME}<br>"
+            },
+            // renderer: {
+            //     type: "simple", // autocasts as new SimpleRenderer()
+            //     symbol: {
+            //         type: "point",  // autocasts as new PointSymbol3D()
+            //         symbolLayers: [{
+            //           type: "icon",  // autocasts as new IconSymbol3DLayer()
+            //           size: 8,  // points
+            //           resource: { primitive: "triangle" },
+            //           material: { color: "red" }
+            //         }]
+            //       },
+            //     visualVariables: {
+            //         axis: "heading",
+            //         type: "rotation",
+            //         field: "WIND_DIRECT",
+            //         rotationType: "geographic"
+            //     }
+            // }
         });
         global.map.add(noaaMetarLayer);
+        // noaaMetarLayer.when(function(whenLayer)
+        // {
+        //     var rotationVisualVariable = {
+        //         axis: "heading",
+        //         type: "rotation",
+        //         field: "WIND_DIRECT",
+        //         rotationType: "geographic"
+        //       };
+        //       whenLayer.renderer.visualVariables = [ rotationVisualVariable ];
+        // });
 
         // https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/MODIS_Thermal_v1/FeatureServer/0
 
-        const renderer = {
-            type: "heatmap",
-            colorStops: [
-              { color: "rgba(63, 40, 102, 0)", ratio: 0 },
-              { color: "#472b77", ratio: 0.083 },
-              { color: "#4e2d87", ratio: 0.166 },
-              { color: "#563098", ratio: 0.249 },
-              { color: "#5d32a8", ratio: 0.332 },
-              { color: "#6735be", ratio: 0.415 },
-              { color: "#7139d4", ratio: 0.498 },
-              { color: "#7b3ce9", ratio: 0.581 },
-              { color: "#853fff", ratio: 0.664 },
-              { color: "#a46fbf", ratio: 0.747 },
-              { color: "#c29f80", ratio: 0.83 },
-              { color: "#e0cf40", ratio: 0.913 },
-              { color: "#ffff00", ratio: 1 }
-            ],
-            maxPixelIntensity: 25,
-            minPixelIntensity: 0
-          };
+        // var renderer = {
+        //     type: "heatmap",
+        //     colorStops: [
+        //         { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+        //         { color: "#472b77", ratio: 0.083 },
+        //         { color: "#4e2d87", ratio: 0.166 },
+        //         { color: "#563098", ratio: 0.249 },
+        //         { color: "#5d32a8", ratio: 0.332 },
+        //         { color: "#6735be", ratio: 0.415 },
+        //         { color: "#7139d4", ratio: 0.498 },
+        //         { color: "#7b3ce9", ratio: 0.581 },
+        //         { color: "#853fff", ratio: 0.664 },
+        //         { color: "#a46fbf", ratio: 0.747 },
+        //         { color: "#c29f80", ratio: 0.83 },
+        //         { color: "#e0cf40", ratio: 0.913 },
+        //         { color: "#ffff00", ratio: 1 }
+        //     ],
+        //     maxPixelIntensity: 25,
+        //     minPixelIntensity: 0
+        // };
 
-        const modisLayer = new FeatureLayer({
+
+        var modisLayer = new FeatureLayer({
             // URL to the service
             url: "https://services9.arcgis.com/RHVPKKiFTONKtxq3/arcgis/rest/services/MODIS_Thermal_v1/FeatureServer/0",
             popupTemplate: {
-                title: "TEST",
+                title: "MODIS Thermal Hotspot",
                 content: "Brightness: {BRIGHTNESS}<br>" + 
-                    "Brightness: {BRIGHTNESS}<br>" + 
+                    "Brightness T31: {BRIGHT_T31}<br>" + 
                     "Scan: {SCAN}<br>" + 
                     "Track: {TRACK}<br>" + 
-                    "Confidence: {CONFIDENCE}<br>"
+                    "Confidence: {CONFIDENCE}<br>" + 
+                    "FRP: {FRP}<br>" + 
+                    "ACQ_DATE: {ACQ_DATE}<br>"
             },
-            renderer: renderer
+            // renderer: renderer
         });
         global.map.add(modisLayer);
 
+        var createParams = {
+            layer: modisLayer,
+            view: global.view,
+            field1: {
+                field: "BRIGHTNESS"
+            },
+            field2: {
+                field: "CONFIDENCE"
+            },
+            focus: "HH", // changes orientation of the legend
+            numClasses: 3 // 3x3 grid (value can also be 2 or 4)
+        };
+
+        relationshipRendererCreator.createRenderer(createParams).then(function (createResponse)
+        {
+            // createResponse.renderer.uniqueValueInfos[0].symbol.color = "#FF0000";
+
+            for(var i=0; i< createResponse.renderer.uniqueValueInfos.length;i++)
+            {
+                var currentUVI = createResponse.renderer.uniqueValueInfos[i];
+                switch(currentUVI.value)
+                {
+                    case "HH":
+                        {
+                            currentUVI.symbol.color = "#FF0000";
+                        }
+                        break;
+                    case "HM":
+                        {
+                            currentUVI.symbol.color = "#800000";
+                        }
+                        break;
+                    case "HL":
+                        {
+                            currentUVI.symbol.color = "#400000";
+                        }
+                        break;
+                    case "MH":
+                        {
+                            currentUVI.symbol.color = "#FFA500";
+                        }
+                        break;
+                    case "MM":
+                        {
+                            currentUVI.symbol.color = "#805e00";
+                        }
+                        break;
+                    case "ML":
+                        {
+                            currentUVI.symbol.color = "#402F00";
+                        }
+                        break;
+                    case "LH":
+                        {
+                            currentUVI.symbol.color = "#FFFF00";
+                        }
+                        break;
+                    case "LM":
+                        {
+                            currentUVI.symbol.color = "#808000";
+                        }
+                        break;
+                    case "LL":
+                        {
+                            currentUVI.symbol.color = "#404000";
+                        }
+                        break;
+                }
+            }
+
+            modisLayer.renderer = createResponse.renderer;
+/*
+            var updateParams = {
+                renderer: createResponse.renderer,
+                field1: {
+                    field: "BRIGHTNESS"
+                },
+                field2: {
+                    field: "CONFIDENCE"
+                },
+                focus: "HH", // changes orientation of the legend
+                numClasses: 3, // 3x3 grid (value can also be 2 or 4)
+                colors:[
+                    "#000000",
+                    "#880000",
+                    "#FF0000",
+                    "#000000",
+                    "#008800",
+                    "#00FF00",
+                    "#000000",
+                    "#000088",
+                    "#0000FF"
+                ]
+            };
+            try{
+                relationshipRendererCreator.updateRenderer(updateParams).then(function(updateResponse)
+                {
+                    modisLayer.renderer = updateResponse.renderer;
+                });
+            }
+            catch(ex)
+            {
+                console.log(ex,ex.stack);
+            }
+            */
+        });
 
         // var meteyeLayer = new WMSLayer({
         //     title: "Wind (MetEye)",
@@ -266,16 +422,27 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseEleva
 
     function MGRStoLatLon(sixCoord)
     {
-        coordinateFormatter.load().then( function(loaded)
+        Promise.all([coordinateFormatter.load(), projection.load()]).then( function(loaded)
         {
-            // var locationPrefix = "56HKG676114";
             var locationPrefix = "56HKG";
-            var locationString = locationPrefix + sixCoord;
-            // var coord = coordinateFormatter.fromMgrs(locationString, SpatialReference.WGS84, "old-180-in-zone-56");
-            var coord = coordinateFormatter.fromMgrs(locationString, SpatialReference.WGS84, "automatic");
-            // console.log(coord);
+            try
+            {
+                var currentWGS84coordinate = projection.project(global.view.center, SpatialReference.WGS84);
+                var currentMGRScoordinate = coordinateFormatter.toMgrs(currentWGS84coordinate, "automatic", 3, false);
+                
+                locationPrefix = currentMGRScoordinate.substring(0,5);
+            // var locationPrefix = "56HKG676114";
+                var locationString = locationPrefix + sixCoord;
+                // var coord = coordinateFormatter.fromMgrs(locationString, SpatialReference.WGS84, "old-180-in-zone-56");
+                var coord = coordinateFormatter.fromMgrs(locationString, SpatialReference.WGS84, "automatic");
+                // console.log(coord);
 
-            global.view.goTo({center: coord, zoom: 15});
+                global.view.goTo({center: coord, zoom: 15});
+            }
+            catch(ex)
+            {
+                console.log(ex,ex.stack);
+            }
         });
         // fetch("http://localhost:8888/viewer/proxy/proxy.ashx?https://geographiclib.sourceforge.io/cgi-bin/GeoConvert?input=56HKG676114700070&zone=56&prec=-2&option=Submit").then( function(fetchResult)
         // {
@@ -288,6 +455,7 @@ require(["esri/Map", "esri/WebMap", "esri/views/MapView", "esri/layers/BaseEleva
         return new Promise( function(resolve, reject)
         {
             fetch("http://localhost:8888/viewer/proxy/proxy.ashx?https://www.livetraffic.com/traffic/hazards/fire.json")
+            // fetch("https://server1.kproxy.com/servlet/redirect.srv/sruj/slsqrnfiumae/srst/p2/traffic/hazards/fire.json")
             .then((response) =>
             {
                 return response.json();
